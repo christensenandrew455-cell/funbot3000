9aec0ee2a9283152217e049e0527f3c6418fb347
christensenandrew455-cell <christensenandrew455@gmail.com>
2025-12-08 10:17:25 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+import './globals.css';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: 'sans-serif' }}>
+      <body>
         {children}
       </body>
     </html>
