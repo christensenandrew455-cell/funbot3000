674d5786fa7b6894a1031bc6523a89b012e900c1
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-01-13 10:44:56 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "app/api/generate/route": [
+        "node_modules/@sparticuz/chromium/**"
+      ]
+    }
+  }
+};
 
 module.exports = nextConfig;
